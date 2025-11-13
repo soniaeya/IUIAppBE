d51e5e65658c72d609869255fd141a265f6dbba1
sonia <sonialxy00@mail.com>
2025-11-13 13:01:43 -0500

    demo 2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import 'react-native-get-random-values';
+import { AppRegistry } from 'react-native';
+import App from './src/app/App';
+import { name as appName } from './app.json';
+
 console.log('Happy developing ✨')
+
+AppRegistry.registerComponent(appName, () => App);
